f7309f95956fe2d7cab1ffba677af24a3edea1f4
Holger Reulen <hreulen@uni-goettingen.de>
2013-11-20 00:00:00 +0000

    version 1.0.1

--- R/sim.event.histories.R
+++ R/sim.event.histories.R
@@ -1,7 +1,7 @@
 sim.event.histories <-
 function(n, all.to.all.bhr.all.beta, max.time=10){
-  hf <- function(x, k){return(x[[k]])}
-  all.possible.from.states <- as.numeric(do.call(c, lapply(all.to.all.bhr.all.beta, 
+  hf <- function(x, k){if(!is.null(x$all.to)){return(x[[k]])}}
+  all.possible.from.states <- as.numeric(do.call(c, lapply(all.to.all.bhr.all.beta,
                                                            FUN=hf, k=1)))
   all.first.from <- sample(all.possible.from.states, size=n, replace=T)
   p <- length(all.to.all.bhr.all.beta[[1]]$all.beta[[1]])
@@ -9,17 +9,24 @@ function(n, all.to.all.bhr.all.beta, max.time=10){
   all.x <- matrix(nrow=n, ncol=p, data=all.x)
   histories <- NULL
   for(i in 1:n){
-    history.i <- sim.single.history(first.entry=0, first.from=all.first.from[i], 
-                                    max.time, all.to.all.bhr.all.beta, 
+    history.i <- sim.single.history(first.entry=0, first.from=all.first.from[i],
+                                    max.time, all.to.all.bhr.all.beta,
                                     x.i = all.x[i, ])
-    history.i <- cbind(history.i, rep(i, nrow(history.i)))
-    for(x.index in 1:p){
-      history.i <- cbind(history.i, rep(all.x[i, x.index], nrow(history.i)))      
+    if(!is.null(nrow(history.i))){
+        history.i <- cbind(history.i, rep(i, 1))
+        for(x.index in 1:p){
+            history.i <- cbind(history.i, rep(all.x[i, x.index], 1))
+        }
+    }else{
+        history.i <- cbind(history.i, rep(i, nrow(history.i)))
+        for(x.index in 1:p){
+            history.i <- cbind(history.i, rep(all.x[i, x.index], nrow(history.i)))
+        }
     }
     histories <- rbind(histories, history.i); rm(history.i)
   }
-  histories.as.list <- list("id" = histories[, 5], "entry" = histories[, 1], 
-                            "exit" = histories[, 2], "from" = histories[, 3], 
+  histories.as.list <- list("id" = histories[, 5], "entry" = histories[, 1],
+                            "exit" = histories[, 2], "from" = histories[, 3],
                             "to" = histories[, 4])
   for(x.index in 1:p){
     histories.as.list[[5+x.index]] <- histories[, 5+x.index]
--- R/sim.single.history.R
+++ R/sim.single.history.R
@@ -1,11 +1,11 @@
 sim.single.history <-
-function(first.entry=0, first.from, max.time, 
+function(first.entry=0, first.from, max.time,
                                all.to.all.bhr.all.beta, x.i){
   current.exit <- current.entry <- first.entry
   current.to <- current.from <- first.from
   history.i <- NULL
   p <- length(x.i)
-  while(current.entry < max.time){
+  while((current.entry < max.time) & (!is.null(all.to.all.bhr.all.beta[[current.from]]$all.to))){
     f.x <- all.to.all.bhr.all.beta[[current.from]]$all.beta
     eta.ij <- rep(0, length(f.x))
     for(oi in 1:length(f.x)){
@@ -14,10 +14,10 @@ function(first.entry=0, first.from, max.time,
         eta.ij[oi] <- eta.ij[oi] + f.now(x.i[ii])
       }
     }
-    current.sim <- sim.to(current.entry, current.from, all.to.all.bhr.all.beta, 
+    current.sim <- sim.to(current.entry, current.from, all.to.all.bhr.all.beta,
                           eta.ij=eta.ij)
-    history.i <- rbind(history.i, 
-                       c(current.sim$entry.ij, current.sim$exit.ij, 
+    history.i <- rbind(history.i,
+                       c(current.sim$entry.ij, current.sim$exit.ij,
                          current.sim$from.ij, current.sim$to.ij))
     current.entry <- current.sim$exit.ij
     current.from <- current.sim$to.ij}
