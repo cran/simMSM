4325fc08d75b6f6494b00bb060d9935056aa87f0
Holger Reulen <hreulen@uni-goettingen.de>
2014-02-09 00:00:00 +0000

    version 1.1.41

--- R/tolongformat.R
+++ R/tolongformat.R
@@ -2,63 +2,63 @@ tolongformat <- function(d, mpl){
   all.d <- d
   d <- d$msm.basics
   ## with pmX:
-#   if(length(all.d) > 3.5){
-#     d <- data.frame(d$msm.basics, d$pmX)
-#     ## number of covariates:
-#     p <- ncol(d) - 7
-#     for(i in 1:nrow(d)){
-#       ## current line:
-#       d.i <- d[i, ]
-#       all.to.i <- as.vector(mpl[[d.i$from]]$all.to)
-#       for(k in 1:length(all.to.i)){
-#         if(!(all.to.i[k] == d.i$to)){
-#           d <- rbind(d, d.i)
-#           d[nrow(d), ]$to <- all.to.i[k]
-#           d[nrow(d), ]$delta <- 0
-#           d[nrow(d), ]$trans <- paste(d[nrow(d), ]$from, all.to.i[k], sep = "")
-#           all.d$pmX <- rbind(all.d$pmX, all.d$pmX[i, ])
-#           all.d$ttsce <- rbind(all.d$ttsce, all.d$ttsce[i, ])
-#           q.old <- d.i$trans
-#           q.new <- d[nrow(d), ]$trans
-#           index.old <- grepl(pattern = paste(".", q.old, sep = ""), names(all.d$ttsce))
-#           index.new <- grepl(pattern = paste(".", q.new, sep = ""), names(all.d$ttsce))
-#           all.d$ttsce[nrow(all.d$ttsce), ] <- 0
-#           all.d$ttsce[nrow(all.d$ttsce), index.new] <- all.d$ttsce[i, index.old]
-#         }
-#       }
-#     }
-#     relevant.order <- order(d$id, d$entry)
-#     d <- d[relevant.order, ]
-#     all.d$pmX <- all.d$pmX[relevant.order, ]
-#   }
+  #   if(length(all.d) > 3.5){
+  #     d <- data.frame(d$msm.basics, d$pmX)
+  #     ## number of covariates:
+  #     p <- ncol(d) - 7
+  #     for(i in 1:nrow(d)){
+  #       ## current line:
+  #       d.i <- d[i, ]
+  #       all.to.i <- as.vector(mpl[[d.i$from]]$all.to)
+  #       for(k in 1:length(all.to.i)){
+  #         if(!(all.to.i[k] == d.i$to)){
+  #           d <- rbind(d, d.i)
+  #           d[nrow(d), ]$to <- all.to.i[k]
+  #           d[nrow(d), ]$delta <- 0
+  #           d[nrow(d), ]$trans <- paste(d[nrow(d), ]$from, all.to.i[k], sep = "")
+  #           all.d$pmX <- rbind(all.d$pmX, all.d$pmX[i, ])
+  #           all.d$ttsce <- rbind(all.d$ttsce, all.d$ttsce[i, ])
+  #           q.old <- d.i$trans
+  #           q.new <- d[nrow(d), ]$trans
+  #           index.old <- grepl(pattern = paste(".", q.old, sep = ""), names(all.d$ttsce))
+  #           index.new <- grepl(pattern = paste(".", q.new, sep = ""), names(all.d$ttsce))
+  #           all.d$ttsce[nrow(all.d$ttsce), ] <- 0
+  #           all.d$ttsce[nrow(all.d$ttsce), index.new] <- all.d$ttsce[i, index.old]
+  #         }
+  #       }
+  #     }
+  #     relevant.order <- order(d$id, d$entry)
+  #     d <- d[relevant.order, ]
+  #     all.d$pmX <- all.d$pmX[relevant.order, ]
+  #   }
   ## without pmX:
   #if(length(all.d) < 3.5){
-    ## number of covariates:
-    p <- ncol(d) - 7
-    for(i in 1:nrow(d)){
-      ## current line:
-      d.i <- d[i, ]
-      all.to.i <- as.vector(mpl[[d.i$from]]$all.to)
-      for(k in 1:length(all.to.i)){
-        if(!(all.to.i[k] == d.i$to)){
-          d <- rbind(d, d.i)
-          d[nrow(d), ]$to <- all.to.i[k]
-          d[nrow(d), ]$delta <- 0
-          d[nrow(d), ]$trans <- paste(d[nrow(d), ]$from, all.to.i[k], sep = "")
-          ## all.d$pmX <- rbind(all.d$pmX, all.d$pmX[i, ])
-          all.d$ttsce <- rbind(all.d$ttsce, all.d$ttsce[i, ])
-          q.old <- d.i$trans
-          q.new <- d[nrow(d), ]$trans
-          index.old <- grepl(pattern = paste(".", q.old, sep = ""), names(all.d$ttsce))
-          index.new <- grepl(pattern = paste(".", q.new, sep = ""), names(all.d$ttsce))
-          all.d$ttsce[nrow(all.d$ttsce), ] <- 0
-          all.d$ttsce[nrow(all.d$ttsce), index.new] <- all.d$ttsce[i, index.old]
-        }
+  ## number of covariates:
+  p <- ncol(d) - 7
+  for(i in 1:nrow(d)){
+    ## current line:
+    d.i <- d[i, ]
+    all.to.i <- as.vector(mpl[[d.i$from]]$all.to)
+    for(k in 1:length(all.to.i)){
+      if(!(all.to.i[k] == d.i$to)){
+        d <- rbind(d, d.i)
+        d[nrow(d), ]$to <- all.to.i[k]
+        d[nrow(d), ]$delta <- 0
+        d[nrow(d), ]$trans <- paste(d[nrow(d), ]$from, all.to.i[k], sep = "")
+        ## all.d$pmX <- rbind(all.d$pmX, all.d$pmX[i, ])
+        all.d$ttsce <- rbind(all.d$ttsce, all.d$ttsce[i, ])
+        q.old <- d.i$trans
+        q.new <- d[nrow(d), ]$trans
+        index.old <- grepl(pattern = paste(".", q.old, sep = ""), names(all.d$ttsce), fixed = TRUE)
+        index.new <- grepl(pattern = paste(".", q.new, sep = ""), names(all.d$ttsce), fixed = TRUE)
+        all.d$ttsce[nrow(all.d$ttsce), ] <- 0
+        all.d$ttsce[nrow(all.d$ttsce), index.new] <- all.d$ttsce[i, index.old]
       }
     }
-    relevant.order <- order(d$id, d$entry)
-    d <- d[relevant.order, ]
-    ## all.d$pmX <- all.d$pmX[relevant.order, ]
+  }
+  relevant.order <- order(d$id, d$entry)
+  d <- d[relevant.order, ]
+  ## all.d$pmX <- all.d$pmX[relevant.order, ]
   #}
   all.d$ttsce <- all.d$ttsce[relevant.order, ]
   all.d$msm.basics <- d
